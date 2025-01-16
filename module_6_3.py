ae66c093aae5c267fae8c8b0bc478e94c65474ec
IriMerkulova <151544565+IriMerkulova@users.noreply.github.com>
2025-01-16 13:18:42 +0300

    Add files via upload
    множественное наследование

---
+++ module_6_3.py
@@ -0,0 +1,70 @@
+# Ошибка эволюции
+import random
+
+class Animal:
+    live = True
+    sound = None
+    _DEGREE_OF_DANGER = 0
+
+    def __init__(self, speed):
+        self.speed = speed
+        self._cords = [0, 0, 0]
+
+    def move(self, dx, dy, dz):
+        cord_z = self._cords[2] + dz * self.speed
+        if cord_z < 0:
+            print("It's too deep, I can't dive:(")
+        else:
+            self._cords[0] += dx * self.speed
+            self._cords[1] += dy * self.speed
+            self._cords[2] = cord_z
+
+    def get_cords(self):
+        print(f'X: {self._cords[0]}, Y: {self._cords[1]}, Z: {self._cords[2]}')
+
+    def attack(self):
+        if self._DEGREE_OF_DANGER < 5:
+            print("Sorry, I'm peaceful:")
+        else:
+            print("Be careful, I'm attacking you 0_0")
+
+    def speak(self):
+        print(self.sound)
+
+class Bird(Animal):
+    beak = True
+
+    def lay_eggs(self):
+        eggs_count = random.randint(1, 4)
+        print(f"Here are(is) {eggs_count} eggs for you")
+
+class AquaticAnimal(Animal):
+    _DEGREE_OF_DANGER = 3
+
+    def dive_in(self, dz):
+        dz = abs(dz)
+        cord_z = self._cords[2] - dz * (self.speed / 2)
+        if cord_z < 0:
+            cord_z = 0
+        self._cords[2] = cord_z
+
+class PoisonousAnimal(Animal):
+    _DEGREE_OF_DANGER = 8
+
+class Duckbill(Bird, AquaticAnimal, PoisonousAnimal):
+    sound = "Click-click-click"
+
+
+db = Duckbill(10)
+
+print(db.live)
+print(db.beak)
+
+db.speak()
+db.attack()
+
+db.move(1, 2, 3)
+db.get_cords()
+db.dive_in(6)
+db.get_cords()
+db.lay_eggs()
